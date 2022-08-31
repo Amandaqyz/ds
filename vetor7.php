5443f05bf79e7be768109fc68049209ae2889d00
Amandaqyz <amandagabriela198106@gmail.com>
2022-08-31 07:59:55 -0300

    sss

---
+++ vetor7.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+ <div>
+ <pre>
+   <?php
+
+   $vetor = [
+       "Naruto",
+       "One piece",
+       "Dragon Ball"
+   ];
+
+    echo var_dump($vetor) . "</br></br>";
+
+    array_push($vetor, "Atack on Titan");
+
+    echo var_dump($vetor);
+    
+      
+   ?>
+   </pre>
+   </div>
+</body>
+</html>
